b11dfcf65e64a560099d6627db9d5a839861c913
Alvaro Bermudez Gamez <alvarobermudezgamez@uma.es>
2023-02-03 12:09:35 +0100

    Refactor class Factorial

--- src/main/java/org/example/Factorial.java
+++ src/main/java/org/example/Factorial.java
@@ -3,10 +3,8 @@ package org.example;
 public class Factorial {
     public int compute(int number) {
         int result;
-        if(number==0 || number==1){
+        if(number==0){
             result = 1;
-        }else if(number==2){
-            result = 2;
         }else{
             return number * compute(number-1);
         }
